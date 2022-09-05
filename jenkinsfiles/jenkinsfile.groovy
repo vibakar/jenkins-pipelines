03841ead6bb33f5586e4cbdda4b8fd7928b540a0
vibakar <vibakar23@gmail.com>
2022-09-05 12:40:26 +0100

    Created jenkinsfile to work with python

---
+++ jenkinsfiles/jenkinsfile.groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent {
+        label "python" #docker agent should have label python in jenkins with a docker image containing java and python3
+    }
+    stages{
+        stage("Stage 1") {
+            steps {
+                sh "python3 --version"
+            }
+        }
+    }
+}
