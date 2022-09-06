947839a0bd2c51d5c755106723531f992c9d715c
vibakar <vibakar23@gmail.com>
2022-09-06 14:20:34 +0100

    Fixed syntax error in jobdsl params

--- jobs/pythonGit.groovy
+++ jobs/pythonGit.groovy
@@ -5,7 +5,7 @@ pipelineJob("python-git") {
         nonStoredPasswordParam("GIT_TOKEN", "Github access token")
         stringParam("GIT_ORGANIZATION", "", "Organization to create a repo")
         stringParam("GIT_REPO_NAME", "", "Repo name to create")
-        booleanParam("GIT_REPO_PRIVACY_SETTING", "", "Check the box to set the repo as private")
+        booleanParam("GIT_REPO_PRIVACY_SETTING", false, "Check the box to set the repo as private")
     }
 
     definition {
