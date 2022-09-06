bfa2819cdd2f2b24aa98a14f837623cffbcc13f6
vibakar <vibakar23@gmail.com>
2022-09-06 12:41:52 +0100

    Added jobdsl

---
+++ jobs/python-git.groovy
@@ -0,0 +1,21 @@
+pipelineJob("python-git") {
+    displayName("Python Git")
+
+    parameters {
+        nonStoredPasswordParam('GIT_TOKEN', '')
+    }
+
+    definition {
+        cpsscm {
+            scm {
+                git {
+                    remote {
+                        url("https://github.com/vibakar/python-pipeline.git")
+                    }
+                    branch("master")
+                }
+            }
+            scriptPath("jenkinsfiles/python-git.groovy")
+        }
+    }
+}
