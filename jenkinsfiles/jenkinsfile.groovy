820c4bba4ac33a00efabda0bf8f599ed76b1472d
vibakar <vibakar23@gmail.com>
2022-09-06 11:52:54 +0100

    Added stages to run python script

--- jenkinsfiles/jenkinsfile.groovy
+++ jenkinsfiles/jenkinsfile.groovy
@@ -6,10 +6,30 @@ pipeline {
     agent {
         label "python" //docker agent should have label python in jenkins with a docker image containing java and python3
     }
+
+    parameters {
+        password(name: 'GIT_TOKEN', defaultValue: '', description: 'Github access token')
+    }
+    environment {
+        GIT_TOKEN = params.GIT_TOKEN
+        GIT_ORG = "vibakarorg"
+        GIT_REPO = "python-repo"
+        GIT_REPO_PRIVACY_SETTING = "Y"
+    }
     stages{
-        stage("Stage 1") {
+        stage("Check Python Version") {
             steps {
                 sh "python3 --version"
+                sh "ls -lrt"
+            }
+        }
+
+        stage("Run Python Script") {
+            steps {
+                writeFile file:'git.py', text:libraryResource("git.py")
+                sh "ls -lrt"
+                sh "python3 -m pip install pygithub"
+                sh "python3 git.py -a createRepo"
             }
         }
     }
